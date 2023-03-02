7e7ffd78d86aa7fa6b511722698b6512c764cb6c
Simona <113423182+simmona92@users.noreply.github.com>
2023-03-02 17:18:47 +0200

    Add files via upload

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import Tasks from '../Tasks/Tasks'
+import Tasks from '../Tasks/Tasks';
+import {FaCheck, FaChevronRight} from "react-icons/fa";
 
 function Task(props) {
-    let {taskTitle, status} = props;
+    let {id, taskTitle, status, changeStatus, deleteTask} = props;
   return (
-    <div class="d-flex">
-        <div class="p-2 flex-grow-1">
-            <div class="alert alert-dark" role="alert">{taskTitle}</div>
+    <div className="d-flex">
+        <div className="p-2 flex-grow-1">
+            <div className= {status? "alert alert-success":"alert alert-dark"}  role="alert">{status? <FaCheck/>:<FaChevronRight/>} {taskTitle} {id}</div>
         </div>
-        <div class="p-2"><button type="button" class="btn btn-secondary">Check</button></div>
-        <div class="p-2"><button type="button" class="btn btn-secondary">Delete</button></div>
+        <div onClick = {() => changeStatus(id)} className="p-2"><button type="button" className="btn btn-secondary">{status? "Done":"Check"}</button></div>
+        <div onClick = {() => deleteTask(id)} className="p-2"><button type="button" className="btn btn-warning">Delete</button></div>
     </div>
   )
 }
 
-export default Task
+export default Task;
--- src/Components/Tasks/Tasks.js
+++ src/Components/Tasks/Tasks.js
@@ -1,26 +1,70 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 import Task from '../Task/Task';
 import { v4 as uuidv4 } from 'uuid';
 
+let data = [
+    {
+        id: 1,
+        taskTitle: "Make a cake",
+        status: false
+    },
+    {
+        id: 2,
+        taskTitle: "No alcohol today",
+        status: false
+    },
+    {
+        id: 3,
+        taskTitle: "Wear shirt that says 'LIFE'",
+        status: false
+    },
+    {
+        id: 4,
+        taskTitle: "Sleep",
+        status: false
+    },
+    {
+        id: 5,
+        taskTitle: "Make TODO list",
+        status: false
+    },
+    {
+        id: 6,
+        taskTitle: "Look at list",
+        status: false
+    },
+    {
+        id: 7,
+        taskTitle: "PANIC!",
+        status: false
+    }
+];
 function Tasks() {
-    let allTasks = [
-        {
-            taskTitle: "Make a cake",
-            status: false
-        },
-        {
-            taskTitle: "No alcohol today",
-            status: false
-        }
-    ];
-    let tasks = allTasks.map((task) => {
+    const [tasks, setTasks] = useState(data);
+    
+    const changeStatus = (id) => {
+        const updateData = [...data];
+        updateData.forEach((task) => {
+            if(task.id == id){
+                task.status = true;
+            };
+        });
+        setTasks(updateData);
+    };
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => 
+            task.id !== id
+        ));
+    }
+    let AllTasks = tasks.map((task) => {
         return (
-            <Task key = {uuidv4()} taskTitle = {task.taskTitle} />
+            <Task id = {task.id} taskTitle = {task.taskTitle} status = {task.status} changeStatus = {changeStatus} deleteTask = {deleteTask}/>
         );
     });
   return (
     <div>
-        {tasks}
+        {AllTasks}
     </div>
   );
 }
